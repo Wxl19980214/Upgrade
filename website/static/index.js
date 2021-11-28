866fa90c2cf9a2dd85d0bfbe4516a9f8200aaad7
AdrianYuxia <31448286+AdrianYuxia@users.noreply.github.com>
2021-11-28 01:28:01 -0500

    this is an update to add a qr code after click share

--- website/static/index.js
+++ website/static/index.js
@@ -1,1 +1,13 @@
+var qrcode = new QRCode("qrcode");
 
+$(document).ready(function() {
+
+});
+
+function showQrcode() {
+
+
+    qrcode.makeCode("http://127.0.0.1:5000/home");
+    $("#qrcode").show();
+
+}
