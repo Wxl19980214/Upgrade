391b4c56a2abb325559ce093d4c2813e81b7223c
chenmx98 <xingchenming@outlook.com>
2021-11-30 00:22:49 -0500

    update edit function

--- website/static/index.js
+++ website/static/index.js
@@ -1,1 +1,20 @@
+<<<<<<< Updated upstream
 
+=======
+var qrcode = new QRCode("qrcode",{});
+
+$(document).ready(function() {
+
+});
+
+function showQrcode() {
+
+
+    qrcode.makeCode("http://127.0.0.1:5000/home");
+    $("#qrcode").show();
+}
+
+function editPost(){
+
+}
+>>>>>>> Stashed changes
